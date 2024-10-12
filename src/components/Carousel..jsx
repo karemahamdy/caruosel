4631f685f8ff65b041e0213887b24d8256bcd1b2
Karema <karemahamdy51@gmail.com>
2024-10-13 02:56:05 +0300

    feat: add next and previos fun

--- src/components/Carousel..jsx
+++ src/components/Carousel..jsx
@@ -1,14 +1,30 @@
+import { useState } from "react"
 import { dataImg } from "../data/data"
 
 function Carousel() {
+ const [imgSlider, setImgSlder] = useState(0)
+
+ function NextImage() {
+  setImgSlder((prev)=> (prev  === dataImg.length - 1 ? 0 : prev + 1))
+ }
+
+ 
+ function PreviousImage() {
+  setImgSlder((prev)=> (prev  === 0 ? dataImg.length - 1 : prev - 1))
+
+  
+ }
+
 
   return (
     <>
      <div className="carousel">
-    {dataImg.map((img ,index) => (
-    <img src={img.src} alt={img.alt} key={index} className=" w-[100%]"/>
-  ))}
+    {/* {dataImg.map((img ,index) => ( */}
+    <img src={dataImg[imgSlider].src} alt={dataImg.alt} key={dataImg[imgSlider].src} className=" w-[100%]"/>
+  {/* ))} */}
     </div>
+    <button onClick={NextImage}>next</button>
+    <button onClick={PreviousImage}> Pervious</button>
     </>
   )
 }
