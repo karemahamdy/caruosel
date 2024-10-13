ac458f8c5ae3746cf30c5dd644a38496ed9239b1
Karema <karemahamdy51@gmail.com>
2024-10-13 03:25:27 +0300

    feat: make arrows as a component

--- src/components/Carousel..jsx
+++ src/components/Carousel..jsx
@@ -1,30 +1,31 @@
 import { useState } from "react"
 import { dataImg } from "../data/data"
+import CarouselArrows from "./caruoselArrow"
 
 function Carousel() {
- const [imgSlider, setImgSlder] = useState(0)
+  const [imgSlider, setImgSlder] = useState(0)
 
- function NextImage() {
-  setImgSlder((prev)=> (prev  === dataImg.length - 1 ? 0 : prev + 1))
- }
+  function NextImage() {
+    setImgSlder((prev) => (prev === dataImg.length - 1 ? 0 : prev + 1))
+  }
 
- 
- function PreviousImage() {
-  setImgSlder((prev)=> (prev  === 0 ? dataImg.length - 1 : prev - 1))
 
-  
- }
+  function PreviousImage() {
+    setImgSlder((prev) => (prev === 0 ? dataImg.length - 1 : prev - 1))
+
+
+  }
 
 
   return (
     <>
-     <div className="carousel overflow-hidden h-96 w-96">
-    {/* {dataImg.map((img ,index) => ( */}
-    <img src={dataImg[imgSlider].src} alt={dataImg.alt} key={dataImg[imgSlider].src} className=" w-[100%] h-[100%] object-cover"/>
-  {/* ))} */}
-    </div>
-    <button onClick={NextImage}>next</button>
-    <button onClick={PreviousImage}> Pervious</button>
+      <div className="carousel overflow-hidden h-96 w-96 relative">
+        {/* {dataImg.map((img ,index) => ( */}
+        <img src={dataImg[imgSlider].src} alt={dataImg.alt} key={dataImg[imgSlider].src} className=" w-[100%] h-[100%] object-cover" />
+        {/* ))} */}
+      </div>
+     
+    <CarouselArrows onNextSlider={NextImage} onPreviousSlider={PreviousImage}/>
     </>
   )
 }
